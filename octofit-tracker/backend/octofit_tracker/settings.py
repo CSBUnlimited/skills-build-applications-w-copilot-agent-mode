8f52ee005d95174dd2542ff97d57d3d5cfc10a26
Chathuranga Basnayake <30438241+CSBUnlimited@users.noreply.github.com>
2026-04-15 23:01:34 +0000

    Update settings and URLs to support Codespace deployment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-443k=grreq22y-i7+(@0r^a1p#4s@m$kaplecsiq^a(cbjpi9v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+import os
+# Allow localhost and Codespace public URL
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
